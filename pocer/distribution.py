396c2ac4d444285bd83dbfa384a56be18e0290f9
gixxerHikaru <yuki.sakurai0617@gmail.com>
2024-10-28 23:44:02 +0900

    Random Distribution

--- pocer/distribution.py
+++ pocer/distribution.py
@@ -1,6 +1,15 @@
+import random
 
 def func():
 
-    deck = ['H2', 'D3', 'C4', 'S5', 'H7']
+    deck = ['H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'H10', 'HJ', 'HQ', 'HK', 'HA',
+            'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'CJ', 'CQ', 'CK', 'CA',
+            'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'DJ', 'DQ', 'DK', 'DA',
+            'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'SJ', 'SQ', 'SK', 'SA',]
+    
+    hands = list(set(deck))
+    print(hands[0], hands[1], hands[2], hands[3], hands[4])
+    return hands
 
-    return deck
+if __name__ == '__main__':
+    func()
--- tests/test_distribution.py
+++ tests/test_distribution.py
@@ -1,27 +1,69 @@
+import re
+
 from pocer.distribution import func
 # テスト使い方
 # 全通し pytest test_distribution.py
 # クラス実行 pytest test_distribution.py::TestDistribution
 
+# Suit Check
+class TestSuitCheck:
+    def test_one(self):
+        pattern = r"[HCDS]"
+        assert re.match(pattern, func()[0])
 
-# Distribution
-class TestDistribution:
-    def test_distribution_one(self):
-        
-        assert 'H2' == func()[0]
+    def test_second(self):
+        pattern = r"[HCDS]"
+        assert re.match(pattern, func()[1])
 
-    def test_distribution_second(self):
-    
-        assert 'D3' == func()[1]
+    def test_third(self):
+        pattern = r"[HCDS]"
+        assert re.match(pattern, func()[2])
 
-    def test_distribution_third(self):
-        
-        assert 'C4' == func()[2]
+    def test_fourth(self):
+        pattern = r"[HCDS]"
+        assert re.match(pattern, func()[3])
 
-    def test_distribution_fourth(self):
-        
-        assert 'S5' == func()[3]
+    def test_fifth(self):
+        pattern = r"[HCDS]"
+        assert re.match(pattern, func()[4])
 
-    def test_distribution_fifth(self):
-        
-        assert 'H7' == func()[4]                    
+# Number Check
+class TestNumberCheck:
+    def test_one(self):
+        for i in range(50):
+            pattern = r".[2345678910JQKA]"
+            assert re.match(pattern, func()[0])
+
+    def test_second(self):
+        for i in range(50):
+            pattern = r".[2345678910JQKA]"
+            assert re.match(pattern, func()[1])
+
+    def test_third(self):
+        for i in range(50):
+            pattern = r".[2345678910JQKA]"
+            assert re.match(pattern, func()[2])
+
+    def test_fourth(self):
+        for i in range(50):
+            pattern = r".[2345678910JQKA]"
+            assert re.match(pattern, func()[3])
+
+    def test_fifth(self):
+        for i in range(50):
+            pattern = r".[2345678910JQKA]"
+            assert re.match(pattern, func()[4])
+
+# Not Same Check
+class TestNotSame:
+    def test_not_same(self):
+        assert func()[0] != func()[1] != func()[2] != func()[3] != func()[4]
+
+    def test_not_same_loop(self):
+        for i in range(300):
+            assert func()[0] != func()[1] != func()[2] != func()[3] != func()[4]
+
+# Not Empty
+class TestNotEmpty:
+    def test_not_empty(self):
+        assert func()[0] != func()[1] != func()[2] != func()[3] != func()[4] != ""

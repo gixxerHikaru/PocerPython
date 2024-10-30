7891b41bce519fc41f779a852f099b21e591ea00
gixxerHikaru <yuki.sakurai0617@gmail.com>
2024-10-30 23:24:53 +0900

    Not Include A Count

--- black_jack/black_jack.py
+++ black_jack/black_jack.py
@@ -9,7 +9,12 @@ def func(cards):
     if(judge_black_jack(check_list[0])):
         return "Black Jack"
 
-    return "You Lose..."
+    score = count_score(check_list[0])
+
+    if(score > 21):
+        return "You Lose..."
+    else:
+        return str(score)
 
 def judge_black_jack(check_number_list):
     count = 0
@@ -23,3 +28,9 @@ def judge_black_jack(check_number_list):
     if(check_number_list[8] == 1 and check_number_list[12] == 1):
         return True
     
+def count_score(check_number_list):
+
+    return (check_number_list[0] * 2 + check_number_list[1] * 3 + check_number_list[2] *
+                4 + check_number_list[3] * 5 + check_number_list[4] * 6 + check_number_list[5] * 7 + check_number_list[6] *
+                8 + check_number_list[7] * 9 + check_number_list[8] * 10 + check_number_list[9] * 10 + check_number_list[10] *
+                10 + check_number_list[11] * 10 + check_number_list[12] * 11)
--- tests/test_black_jack.py
+++ tests/test_black_jack.py
@@ -22,6 +22,76 @@ class TestBlackJack:
         cards = ['DA', 'S10']
         assert "Black Jack" == func(cards)        
 
+# Not Include A
+class TestNotIncludeA():
+    def test_4(self):
+        cards = ['H2', 'C2']
+        assert "4" == func(cards)
+
+    def test_5(self):
+        cards = ['C2', 'C3']
+        assert "5" == func(cards)
+
+    def test_6(self):
+        cards = ['H3', 'C3']
+        assert "6" == func(cards)
+
+    def test_7(self):
+        cards = ['D3', 'S4']
+        assert "7" == func(cards)
+
+    def test_8(self):
+        cards = ['H4', 'C4']
+        assert "8" == func(cards)
+
+    def test_9(self):
+        cards = ['C2', 'C7']
+        assert "9" == func(cards)
+
+    def test_10(self):
+        cards = ['H5', 'C5']
+        assert "10" == func(cards)
+
+    def test_11(self):
+        cards = ['D5', 'S6']
+        assert "11" == func(cards)
+
+    def test_12(self):
+        cards = ['H6', 'C6']
+        assert "12" == func(cards)
+
+    def test_13(self):
+        cards = ['C6', 'C7']
+        assert "13" == func(cards)
+
+    def test_14(self):
+        cards = ['H7', 'C7']
+        assert "14" == func(cards)
+
+    def test_15(self):
+        cards = ['D7', 'S8']
+        assert "15" == func(cards)
+
+    def test_16(self):
+        cards = ['H8', 'C8']
+        assert "16" == func(cards)
+
+    def test_17(self):
+        cards = ['C8', 'C9']
+        assert "17" == func(cards)
+
+    def test_18(self):
+        cards = ['H9', 'C9']
+        assert "18" == func(cards)
+
+    def test_19(self):
+        cards = ['D9', 'SJ']
+        assert "19" == func(cards)
+
+    def test_20(self):
+        cards = ['DJ', 'SJ']
+        assert "20" == func(cards)
+
 # Lose
 class TestLose:
     def test_22(self):

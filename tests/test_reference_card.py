49f352663189f1b42f36f59bb82db48285b3a06a
gixxerHikaru <yuki.sakurai0617@gmail.com>
2025-01-21 22:00:58 +0900

    画像パス生成コード

---
+++ tests/test_reference_card.py
@@ -0,0 +1,13 @@
+import re
+
+from pocer.reference_card import func
+
+class TestSuitCheck:
+    def test_club_one(self):
+        card = "CA"
+        pattern = "/Users/sakuraiyuuki/Documents/GitHub/PocerPython/pocer/image/cards/CA.png"
+        assert re.match(pattern, func(card))
+    def test_club_second(self):
+        card = "C2"
+        pattern = "/Users/sakuraiyuuki/Documents/GitHub/PocerPython/pocer/image/cards/C2.png"
+        assert re.match(pattern, func(card))        
